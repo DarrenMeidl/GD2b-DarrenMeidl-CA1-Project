64c50b71619ec06c6c8ad4d3c260ca783d259cab
Darren <dmeidl15@gmail.com>
2023-10-11 14:53:22 +0100

    Created flip function

--- Assets/Scripts/AdvancedPlayerMovement.cs
+++ Assets/Scripts/AdvancedPlayerMovement.cs
@@ -11,7 +11,7 @@ public class AdvancedPlayerMovement : MonoBehaviour
     private bool grounded;
     private bool facingRight = true;
     // Start is called before the first frame update
-    void Awake()
+    private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
@@ -20,17 +20,31 @@ public class AdvancedPlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
         if (Input.GetKey(KeyCode.Space) && grounded){
             Jump();
         }
+
+        if((horizontalInput > 0 && !facingRight) || (horizontalInput < 0 && facingRight)){
+            Flip();
+        }
     }
+
+    private void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
+    }
+
     private void Jump(){
         body.velocity = new Vector2(body.velocity.x, jumpHeight);
         grounded = false;
     }
+
     private void OnCollisionEnter2D(Collision2D other){
         if(other.gameObject.CompareTag("Ground")){
             grounded = true;
