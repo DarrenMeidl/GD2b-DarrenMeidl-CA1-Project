4fcce698eb658ea3070f2d9ec0978ff3ff42e6ad
DarrenMeidl <dmeidl15@gmail.com>
2023-10-17 19:24:04 +0100

    Commented code in AdvancedPlayerMovement script to disable it, duplicated original Player object as a backup, created Player script

--- Assets/Scripts/PogoStickParent.cs
+++ Assets/Scripts/PogoStickParent.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,3 +11,4 @@ public class PogoStickParent : MonoBehaviour
         transfor.right = (PointerPosition-(Vector2)transform.position).normalized;
     }
 }
+*/
