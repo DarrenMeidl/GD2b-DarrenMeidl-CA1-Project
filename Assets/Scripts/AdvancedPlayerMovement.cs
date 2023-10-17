876ba5c6bb6c957df0a136e79ccf8c88f7f2aa42
DarrenMeidl <dmeidl15@gmail.com>
2023-10-17 22:09:29 +0100

    Deleted all new input system code & objects, returning to old input system

--- Assets/Scripts/AdvancedPlayerMovement.cs
+++ Assets/Scripts/AdvancedPlayerMovement.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -60,4 +60,3 @@ public class AdvancedPlayerMovement : MonoBehaviour
     }
 
 }
-*/
