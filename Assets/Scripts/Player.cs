ec8380c3bf3f5d7e5e573e4bf236f99bc69e6ecb
DarrenMeidl <dmeidl15@gmail.com>
2023-10-17 19:28:22 +0100

    Created Serialized Field for movement, shoot & pointer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField]
+    private InputActionReference movement, shoot, pointerPosition;
     // Start is called before the first frame update
     void Start()
     {
