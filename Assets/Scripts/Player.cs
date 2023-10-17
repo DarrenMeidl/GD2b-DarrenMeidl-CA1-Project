79b15578b508b69919f0b713a0221d22f515dec2
DarrenMeidl <dmeidl15@gmail.com>
2023-10-17 20:21:27 +0100

    Commented out old input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,18 +11,21 @@ public class Player : MonoBehaviour
     public float dashSpeed = 20f;
     public float crouchHeight = .5f;
     public float groundCheckRadius = 0.2f;
-    float horizontalInput;
-    float pointerInput;
     public Transform groundCheckPoint;
     public LayerMask whatIsGround;
 
-    private Rigidbody2D body;
-    private Animator anim;
     private bool grounded;
     private bool canDoubleJump = false;
     private bool isDashing = false;
     private bool isCrouching = false;
     private bool facingRight = true;
+
+    private Vector2 horizontalInput, pointerInput;
+
+    public Vector2 PointerInput => pointerInput;
+
+    private Rigidbody2D body;
+    private Animator anim;
 
     [SerializeField]
     private InputActionReference movement, attack, pointerPosition;
@@ -35,33 +38,33 @@ public class Player : MonoBehaviour
     }
 
     void Update(){
-        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+        //grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
         
         pointerInput = GetPointerInput();
 
         horizontalInput = movement.action.ReadValue<Vector2>();
-        anim.SetBool("walk", horizontalInput !=0);
+        //anim.SetBool("walk", horizontalInput !=0);
 
-        if((horizontalInput > 0 && !facingRight) || (horizontalInput < 0 && facingRight)){
+        /*if((horizontalInput > 0 && !facingRight) || (horizontalInput < 0 && facingRight)){
             Flip();
-        }
+        }*/
     }
 
-    private void Flip(){
+    /*private void Flip(){
         Vector3 currentScale = gameObject.transform.localScale;
         currentScale.x *= -1;
         gameObject.transform.localScale = currentScale;
         facingRight = !facingRight;
-    }
+    }*/
 
-    private void Jump(){
+    /*private void Jump(){
         body.velocity = new Vector2(body.velocity.x, jumpHeight);
         grounded = false;
         anim.SetTrigger("jump");
-    }
+    }*/
 
     private Vector2 GetPointerInput(){
-        Vector3 mousPos = pointerPosition.action.ReadValue<Vector2>();
+        Vector3 mousePos = pointerPosition.action.ReadValue<Vector2>();
         mousePos.z = Camera.main.nearClipPlane;
         return Camera.main.ScreenToWorldPoint(mousePos);
     }
