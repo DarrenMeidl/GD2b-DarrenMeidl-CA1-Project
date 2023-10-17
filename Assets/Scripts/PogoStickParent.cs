634c4e6ee28044b5a9dc1acd917eead199a5ffa3
DarrenMeidl <dmeidl15@gmail.com>
2023-10-17 18:52:59 +0100

    Installed New Input System package & created New Input Actions

--- Assets/Scripts/PogoStickParent.cs
+++ Assets/Scripts/PogoStickParent.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class PogoStickParent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public Vector2 Pointerposition { get; set; }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update(){
+        transfor.right = (PointerPosition-(Vector2)transform.position).normalized;
     }
 }
