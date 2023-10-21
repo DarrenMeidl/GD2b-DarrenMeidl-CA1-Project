4304e97c7befffd1519dd622da15e88b96ebe055
DarrenMeidl <dmeidl15@gmail.com>
2023-10-21 16:40:50 +0100

    Created GameOver Menu & script, moved functions over from GameManager script to GameOverMenu script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,6 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance; //This is a singleton, this same variable that can be called across all scripts
 
-    [Header("Game States")] //public bools to determine if game is over
-    public bool isGameOver;
     // Awake is called before the game starts
     void Awake()
     {
@@ -26,22 +24,9 @@ public class GameManager : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
-        if(Input.GetKeyDown(KeyCode.R)){ //If the player hits 'R' then restart the game (this key bind is temporary)
-            RestartGame();
-        }
 
     }   
     
-    //This function ends the game
-    public void GameOver()
-    {
-        isGameOver=true; //Sets the game over bool to true
-        //This is where I'll put in code for when the game ends
-    }
-    //This function restarts the game
-    public void RestartGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Loads a scene by index, the index comes from the current active scene index
-    }
+    
     
 }
---
+++ Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//Repurposed code from unity tutorial, link: https://www.youtube.com/watch?v=9dYDBomQpBQ&ab_channel=BMo
+public class GameOverMenu : MonoBehaviour
+{
+    public GameObject gameoverMenu; //Used to reference GameOverMenu object
+    public bool isDead; //public bool to determines if game is paused or not
+    // Awake is called before the game starts
+    void Awake(){ //Player is not dead, time is set to normal & GameOverMenu object will be hidden before game starts
+        isDead = false;
+        Time.timeScale = 1f;
+        gameoverMenu.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.T)){ //If player hits T key, executes GameOver() function (testing purposes)
+            GameOver();
+        }
+        
+    }
+    //This function ends the game
+    public void GameOver()
+    {
+        isDead=true; //Sets the game over bool to true
+        gameoverMenu.SetActive(true); //Activates Death Screen
+        Time.timeScale = 0f; //Stops time
+    }
+
+    //This function restarts the game
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //Loads a scene by index, the index comes from the current active scene's index
+    }
+
+    //This function loads the main menu scene, closes the pause menu if it's still open & sets isPaused to false
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        gameoverMenu.SetActive(false);
+        SceneManager.LoadScene("Main Menu"); //Loads the scene by name
+        isDead = false;
+    }
+
+    
+    
+}
+
