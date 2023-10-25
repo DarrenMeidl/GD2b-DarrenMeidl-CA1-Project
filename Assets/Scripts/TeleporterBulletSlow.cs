4f72ea0bdd1a70c5944655422a6550e6dac2acc0
DarrenMeidl <dmeidl15@gmail.com>
2023-10-25 09:31:48 +0100

    Teleporter Bullet can change direction infinitely, speeds adjusted

--- Assets/Scripts/TeleporterBulletSlow.cs
+++ Assets/Scripts/TeleporterBulletSlow.cs
@@ -24,8 +24,8 @@ public class TeleporterBulletSlow : MonoBehaviour
     }
 
     void Update(){
-            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        if(Input.GetKeyDown(KeyCode.F) && hasContactedSlow == false){
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //Sets direction vector to wherever the mouse is located
+        if(Input.GetKeyDown(KeyCode.F)){ //If the player hits F, the bullet will change to the direction of direction variable
             rb.velocity = direction.normalized * bulletSpeed;
         }
     }
